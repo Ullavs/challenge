b228688b73679f118fcfd5685109100a2ce71c63
Ulla <ullavansprang@gmail.com>
2024-09-18 17:42:39 +0200

    Connect nextauth to prisma

--- src/components/SignIn/index.tsx
+++ src/components/SignIn/index.tsx
@@ -1,6 +1,24 @@
-import { signIn } from "@/auth";
+import { auth, signIn, signOut } from "@/auth";
 
-export default function SignIn() {
+export default async function SignIn() {
+  const session = await auth();
+
+  if (session?.user) {
+    return (
+      <div>
+        <h1>Hello {session.user.name}</h1>
+        <form
+          action={async () => {
+            "use server";
+            await signOut();
+          }}
+        >
+          <button type="submit">Sign out</button>
+        </form>
+      </div>
+    );
+  }
+
   return (
     <form
       action={async () => {
